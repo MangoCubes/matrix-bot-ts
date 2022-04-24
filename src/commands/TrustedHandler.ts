aa5469848b38b47194377f0a3e6de736c1c46a8d
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2022-04-24 11:24:01 +0000

    Using yargs for trusted handler

--- src/commands/TrustedHandler.ts
+++ src/commands/TrustedHandler.ts
@@ -1,36 +1,49 @@
-import minimist from 'minimist';
+import yargs from "yargs/yargs";
 import CommandHandler from "./CommandHandler";
 
 export default class TrustedHandler extends CommandHandler{
 	async handleMessage(command: readonly string[], sender: string, roomId: string): Promise<void> {
 		if(command[0] !== this.prefix) return;
-		const args = minimist(command.slice(1), {
-			boolean: ['overwrite', 'add', 'remove'],
-			alias: {
-				'overwrite': 'o',
-				'add': 'a',
-				'remove': 'r'
+		const cmd = yargs().scriptName('!trust').help(false).exitProcess(false).command(['add', 'a'], 'Add new trusted users', async (cmd) => {
+			const args = await cmd.string(['_']).option('o', {
+				alias: 'overwrite',
+				type: 'boolean'
+			}).parseAsync(command.slice(2));
+			const input = this.normaliseNames(args._);
+			let newUserList = [];
+			if(args.o) newUserList = [...input];
+			else {
+				const tempSet = new Set(this.client.trusted.trusted.concat(input));
+				newUserList = Array.from(tempSet);
 			}
-		});
-		const input = this.normaliseNames(args._);
-		let newUserList = [];
-		if(args.overwrite) newUserList = [...input];
-		else if(args.remove) {
+			await this.changeUsers(newUserList);
+			await this.client.sendMessage(roomId, `The following users have been added to the trusted list:\n${this.client.trusted.trusted.join('\n')}`);
+		}).command(['remove', 'r'], 'Remove trusted users', async (cmd) => {
+			const args = await cmd.parseAsync(command.slice(2));
+			const input = this.normaliseNames(args._);
+			let newUserList = [];
 			const tempSet = new Set<string>(this.client.trusted.trusted);
 			for(const u of input) tempSet.delete(u);
 			newUserList = Array.from(tempSet);
+			await this.changeUsers(newUserList);
+			await this.client.sendMessage(roomId, `The following users have been removed to the trusted list:\n${input.join('\n')}`);
+		}).command(['list', 'ls'], 'List trusted users', async (cmd) => {
+			await this.client.sendMessage(roomId, 'List of trusted users:\n' + this.client.trusted.trusted.join('\n'));
+		}).option('h', {
+			alias: 'help',
+			type: 'boolean',
+		});
+		const args = await cmd.parseAsync(command.slice(1));
+		if(args.h){
+			await this.client.sendMessage(roomId, await cmd.getHelp());
+			return;
 		}
-		else {
-			const tempSet = new Set(this.client.trusted.trusted.concat(input));
-			newUserList = Array.from(tempSet);
-		}
-		await this.changeUsers(newUserList);
-		await this.client.sendMessage(roomId, `The following users have been added to the trusted list:\n${this.client.trusted.trusted.join(', ')}`);
 	}
 
-	normaliseNames(usernames: string[]){
+	normaliseNames(usernames: (string | number)[]){
 		let users = [];
 		for(const u of usernames){
+			if(typeof u === 'number') continue;
 			let user = u.trim();
 			if(!user.length) continue;
 			if(!user.startsWith('@')) user = '@' + user;
