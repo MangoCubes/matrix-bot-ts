e4976453aa0d17aea7fb579c8df8e25529ca55ca
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2022-04-26 14:04:53 +0000

    Better parsing of quote characters

--- src/class/Command.ts
+++ src/class/Command.ts
@@ -6,9 +6,15 @@ export default class Command{
 	ignore: string[];
 	constructor(command: string | string[], ignore: string[]){
 		if(typeof(command) === 'string'){
-			const split = command.match(/\w+|"(?:\\"|[^"])+"/g);
+			const split = command.match(/\w+|"(\\"|[^"])+"/g);
 			if(!split) throw new ParsingError('Invalid quotes.');
-			else this.command = split;
+			else {
+				this.command = split;
+				for(let i = 0; i < this.command.length; i++){
+					const match = this.command[i].match(/^"(.+)"$/);
+					if(match) this.command[i] = match[1];
+				}
+			}
 		}else this.command = command;
 		this.ignore = ignore;
 	}
