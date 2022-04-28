d10fd2a038e7fdf66307f3277191c82577efc88e
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2022-04-28 14:22:10 +0000

    Improved regex, should work with ' as well as "

--- src/class/Command.ts
+++ src/class/Command.ts
@@ -8,15 +8,18 @@ export default class Command{
 	constructor(command: string | string[], eventId: string | undefined, ignore: string[]){
 		this.eventId = eventId;
 		if(typeof(command) === 'string'){
-			const split = command.match(/[^"\s]+|"(?:\\"|[^"])+"/g);
+			const split = command.match(/[^"'\s]+|["'](?:\\"|[^"]|\\'|[^'])+["']/g);
 			if(!split) throw new ParsingError('Invalid quotes.');
 			else {
 				this.command = split;
 				for(let i = 0; i < this.command.length; i++){
-					const match = this.command[i].match(/^"(.+)"$/);
+					const match = this.command[i].match(/^["'](.+)["']$/);
 					if(match) this.command[i] = match[1];
+					this.command[i] = this.command[i].replace(/\\"/g, '"');
+					this.command[i] = this.command[i].replace(/\\'/g, "'");
 				}
 			}
+			console.log(this.command)
 		}else this.command = command;
 		this.ignore = ignore;
 	}
