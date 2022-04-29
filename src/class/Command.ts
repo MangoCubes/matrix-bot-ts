3f7ab44c5e041a8556e852bba6a4a8c3c2be47b8
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2022-04-29 11:12:50 +0000

    Separated command parser

--- src/class/Command.ts
+++ src/class/Command.ts
@@ -7,33 +7,8 @@ export default class Command{
 	eventId: string | undefined;
 	constructor(command: string | string[], eventId: string | undefined, ignore: string[]){
 		this.eventId = eventId;
-		if(typeof(command) === 'string'){
-			const split = command.match(/[^"'\s]+|(["'])(?:\\"|[^"]|\\'|[^'])+\1/g);
-			/**
-			 * Regex explanation
-			 * 1st alternative: [^"'\s]+
-			 * 	- Matches anything that doesn't contain whitespace, ' or "
-			 * 2nd alternative: ["'](?:\\"|[^"]|\\'|[^'])+["']
-			 * 	- (["']): First capture group, matches a quotemark start
-			 *  - (?:\\"|[^"]|\\'|[^']): Non-capturing group, captures one of the following:
-			 * 		- \\": Captures \", escaped quotemark
-			 * 		- [^"]: Anything that isn't "
-			 * 		- \\': Captures \', escaped quotemark
-			 * 		- [^']: Anything that isn't '
-			 * 	- +: Capture 1+ of these, in a greedy manner
-			 * 	- \1: Backreference to first capture group, which is a closing quotemark
-			 */
-			if(!split) throw new ParsingError('Invalid quotes.');
-			else {
-				this.command = split;
-				for(let i = 0; i < this.command.length; i++){
-					const match = this.command[i].match(/^["'](.+)["']$/);
-					if(match) this.command[i] = match[1];
-					this.command[i] = this.command[i].replace(/\\"/g, '"');
-					this.command[i] = this.command[i].replace(/\\'/g, "'");
-				}
-			}
-		} else this.command = command;
+		if(typeof(command) === 'string') this.command = commandParser(command);
+		else this.command = command;
 		this.ignore = ignore;
 	}
 	getName() {
@@ -44,5 +19,36 @@ export default class Command{
 	}
 	getOptions() {
 		return this.command.slice(1);
+	}
+}
+
+export function commandParser(command: string){
+	const split = command.match(/(?:\\\\"|\\\\'|[^"'\s])+|(["'])(?:\\"|[^"]|\\'|[^'])+?\1/g);
+	/**
+	 * Regex explanation
+	 * 1st alternative: [^"'\s]+
+	 * 	- Matches anything that doesn't contain whitespace, ' or "
+	 * 2nd alternative: ["'](?:\\"|[^"]|\\'|[^'])+["']
+	 * 	- (["']): First capture group, matches a quotemark start
+	 *  - (?:\\"|[^"]|\\'|[^']): Non-capturing group, captures one of the following:
+	 * 		- \\": Captures \", escaped quotemark
+	 * 		- [^"]: Anything that isn't "
+	 * 		- \\': Captures \', escaped quotemark
+	 * 		- [^']: Anything that isn't '
+	 * 	- +: Capture 1+ of these, in a greedy manner
+	 * 	- \1: Backreference to first capture group, which is a closing quotemark
+	 */
+	if(!split) throw new ParsingError('Invalid quotes.');
+	else {
+		let arr = split;
+		console.log(arr);
+		for(let i = 0; i < arr.length; i++){
+			const match = arr[i].match(/^["'](.+)["']$/);
+			if(match) arr[i] = match[1];
+			arr[i] = arr[i].replace(/\\"/g, '"');
+			arr[i] = arr[i].replace(/\\'/g, "'");
+		}
+		
+		return arr;
 	}
 }
